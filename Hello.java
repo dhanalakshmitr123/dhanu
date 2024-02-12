3f69d4d273e653eb2db8b2a0287112ea238bc6d5
nir <nirmalrange@gmail.com>
2024-02-12 15:52:18 +0530

    version1.2

--- Hello.java
+++ Hello.java
@@ -2,9 +2,10 @@ public class Hello
 {
 	public Static void main(String [] args)
 	{
-		system.out.println("Hello tumkur");
-		system.out.println("benglore");
+		system.out.println("Hello tumkur..");
+		system.out.println("benglore..");
 		system.out.println("gubbi..");
+		system.out.println("mysore");
 
 	}
 }
